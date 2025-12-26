0c4691025a43f554d9f669d1ed2a468c58285105
Jirrer <jrirrer@gmail.com>
2025-12-26 00:38:23 -0500

    created map and layout. created script to convert old html

--- public/src/App.jsx
+++ public/src/App.jsx
@@ -2,32 +2,21 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import TopBar from './components/TopBar'
+import Map from './components/Map'
+import Charts from './components/Charts'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div className="AppGrid">
+      <div className='TopBar'><TopBar /></div>
+      <div className='LeftSide'><Map /></div>
+      <div className='RightSide'><Charts /></div>
+    </div>
+    
+   
     </>
   )
 }
