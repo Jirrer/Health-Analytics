f6786229fc8d68141609f20593d651a842c9803c
Jirrer <jrirrer@gmail.com>
2026-01-08 18:00:44 -0500

    added new pulldata

--- src/PullData.py
+++ src/PullData.py
@@ -1,6 +1,29 @@
 import json
 from src.Methods import makeSingleQuery, groupByCounty
 
+def pullDeath_Birth(county) -> tuple[bool, dict]: 
+    deathsQuery = f"SELECT name, year, Deaths FROM counties WHERE name = '{county}' ORDER BY name, year"
+
+    birthsQuery = f"SELECT name, year, Births FROM counties WHERE name = '{county}' ORDER BY name, year"
+
+    deathsQueryStatus, deathsQueryResponse = makeSingleQuery(deathsQuery)
+
+    birthsQueryStatus, birthsQueryResponse = makeSingleQuery(birthsQuery)
+
+    if not deathsQueryStatus: 
+        return (False, deathsQueryResponse)
+    
+    if not birthsQueryStatus:
+        return (False, birthsQueryResponse)
+
+    deathsCounties = groupByCounty(deathsQueryResponse)
+
+    birthsCounties = groupByCounty(birthsQueryResponse)
+
+    return (True, {'Deaths': deathsCounties, 'Births': birthsCounties})
+
+pullDeath_Birth('Clinton')
+
 def pullMedianIncome(county) -> tuple[bool, dict]: 
     query = f"SELECT name, year, Median_Income FROM counties WHERE name = '{county}' ORDER BY name, year"
 
