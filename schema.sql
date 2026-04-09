d6f1a61e9fb7d2013eaf92a3a770f67d9666d159
Jirrer <jrirrer@gmail.com>
2026-04-08 21:11:57 -0400

    added database scema

---
+++ schema.sql
@@ -0,0 +1,2 @@
+CREATE TABLE counties(name text not null, year integer, median_income integer, health_rank integer, Gini_Coeffient real, Deaths integer, Births integer);
+CREATE TABLE info (calculation text, content text);
