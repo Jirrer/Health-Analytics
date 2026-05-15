6f3c04f92dc79d15ab9ffd3c556f1fbcc26ae5fb
Jirrer <jrirrer@gmail.com>
2026-05-15 17:05:18 -0400

    clearing gh-pages

--- public/vite.config.js
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react'; // or vue, svelte, etc.
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/health_analytics/',   // <-- this is the key
-});
